d878fde074c61ac71a524fd78fff99a377db1d47
Geert Van Huychem <geert@iframeworx.be>
2019-06-07 07:49:51 +0200

    first commit

---
+++ MvcStarter/Controllers/LocalizationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+
+namespace MvcStarter.Controllers
+{
+    public class LocalizationController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            ViewData["culture"] = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+            ViewData["controller"] = ControllerContext.ActionDescriptor.ControllerName;
+            ViewData["action"] = ControllerContext.ActionDescriptor.ActionName;
+        }
+    }
+}
---
+++ MvcStarter/Startup.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace MvcStarter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("nl-BE"),
+                    new CultureInfo("fr-BE")
+                };
+
+                options.DefaultRequestCulture = new RequestCulture(culture: "nl-BE", uiCulture: "nl-BE");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+                options.RequestCultureProviders = new List<IRequestCultureProvider>(
+                    new[]
+                    {
+                        new RouteDataRequestCultureProvider
+                        {
+                            IndexOfCulture = 1,
+                            IndexofUiCulture = 1
+                        }
+                    }
+                );
+            });
+
+            services.Configure<RouteOptions>(options =>
+            {
+                options.ConstraintMap.Add("culture", typeof(LanguageRouteConstraint));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "LocalizedDefault",
+                    template: "{culture:culture}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{*catchall}",
+                    defaults: new { controller = "Home", action = "RedirectToDefaultCulture", culture = "nl" });
+            });
+        }
+    }
+
+    public class LanguageRouteConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if (!values.ContainsKey("culture"))
+                return false;
+
+            var culture = values["culture"].ToString();
+
+            return culture == "nl" || culture == "fr";
+        }
+    }
+
+    public class RouteDataRequestCultureProvider : RequestCultureProvider
+    {
+        public int IndexOfCulture;
+        public int IndexofUiCulture;
+
+        public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+        {
+            if (httpContext == null)
+                throw new ArgumentNullException(nameof(httpContext));
+
+            string culture = null;
+            string uiCulture = null;
+
+            var twoLetterCultureName = httpContext.Request.Path.Value.Split('/')[IndexOfCulture];
+            var twoLetterUiCultureName = httpContext.Request.Path.Value.Split('/')[IndexofUiCulture];
+
+            if (twoLetterCultureName == "nl")
+                culture = "nl-BE";
+            else if (twoLetterCultureName == "fr")
+                culture = uiCulture = "fr-BE";
+
+            if (twoLetterUiCultureName == "nl")
+                culture = "nl-BE";
+            else if (twoLetterUiCultureName == "fr")
+                culture = uiCulture = "fr-BE";
+
+            if (culture == null)
+                return NullProviderCultureResult;
+
+            if (uiCulture == null)
+                uiCulture = culture;
+
+            var providerResultCulture = new ProviderCultureResult(culture, uiCulture);
+
+            return Task.FromResult(providerResultCulture);
+        }
+    }
+}
